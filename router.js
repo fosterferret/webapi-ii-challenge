381cb2c51e367ed11a1fe29e53474499629f4b38
Francis Bulus <notchera@gmail.com>
2019-10-29 15:46:18 +0100

    implemented delete operation

--- router.js
+++ router.js
@@ -57,4 +57,21 @@ router.post("/:id/comments", (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  posts
+    .remove(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(req.params.id);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The post could not be removed." })
+    );
+});
+
 module.exports = router;
