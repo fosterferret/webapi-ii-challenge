6eec969dc40818a61d66dcaa562402168592c679
Francis Bulus <notchera@gmail.com>
2019-10-29 15:09:28 +0100

    created server and implememnt post request

--- router.js
+++ router.js
@@ -1,4 +1,20 @@
-const express = require ('express');
-const Posts = require('./data/db');
-const router = express.Router()
+const express = require("express");
+const Posts = require("./data/db");
+const router = express.Router();
 
+router.post("/", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide the title and contents of the post."
+    });
+  } else {
+    Posts.insert(req.body)
+      .then(post => res.status(201).json(req.body))
+      .catch(err =>
+        res.status(500).json({
+          error:
+            "We encountered an error while saving this post to the database."
+        })
+      );
+  }
+});
