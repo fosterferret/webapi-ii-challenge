b3b7c3dc226115b56f551a66d3953beaebe82348
Francis Bulus <notchera@gmail.com>
2019-10-29 15:50:32 +0100

    implemented put operation for posts

--- router.js
+++ router.js
@@ -124,4 +124,28 @@ router.get("/:id/comments", (req, res) => {
     );
 });
 
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    posts.update(req.params.id, req.body)
+      .then(post => {
+        if (post) {
+          res.status(200).json({ ...req.body, id: req.params.id });
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "The post information could not be modified." })
+      );
+  }
+});
+
 module.exports = router;
